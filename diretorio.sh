6539620f9369a5cda052193651859dc0c832c3db
Thiago Dias <diasthi31@gmail.com>
2023-08-18 17:39:34 -0300

    Update diretorio.sh

--- diretorio.sh
+++ diretorio.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
 
-diretorio=~/LFTnoite/ex1Script
+while true
+do
+echo -n "Qual animal você está vendo ? "
+read animal
+echo 
 
-if [ -e "$diretorio" ]; then
-  echo "Diretório já existente!"
-else
-  echo "Diretório não existe, deseja criá-lo [s/n]?"
-  read resposta
-  if [ "$resposta" = "n" ]; then
-    exit
-  else 
-    mkdir -p "$diretorio"
-  fi
-fi
+case $animal in
+"leao" | "tigre")
+echo "É melhor correr!"
+;;
+"gato")
+echo "Pode ser útil para pegar ratos."
+;;
+"cachorro")
+echo "Não se preocupe, dê um biscoito para ele."
+;;
+"galinha" | "ganso" | "pato" )
+echo "Beleza! Ovos para o almoço!"
+;;
+"vaca")
+echo "Leite para o café da manhã...'."
+;;
+*)
+echo "Animal não reconhecido pelo sistema!"
+;;
+esac
+
+echo "Se quiser parar, aperte [CTRL+C]."
+echo
+done
